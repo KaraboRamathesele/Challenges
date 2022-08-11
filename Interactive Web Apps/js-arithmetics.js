4fa5239647b7a2d2c4c2a4a8f762bd38f07022ab
KaraboRamathesele <karaboramathesele8115@gmail.com>
2022-08-11 21:56:04 +0200

    JS arithmetic
    - OUTPUT TO BROWSER?

---
+++ Interactive Web Apps/js-arithmetics.js
@@ -0,0 +1,42 @@
+console.log(" ----- Area Of Shapes JS Arithmetic Challenge ----- \n\n")
+
+// Constant Variables:
+const pi = 3.1419;
+const half = 0.5;
+
+// Variables for measurements
+
+   let length = 10;
+   let breadth = 17;
+   let height = 9;
+   let base = 20; 
+   let radius =25;
+
+
+// Rectangle area calculation
+   rectangleArea = length * height;
+
+
+// Append your rectangle answer:
+console.log("The area of the given rectangle is : " + rectangleArea);
+
+
+
+console.log("\n");
+
+
+// circle area calculation
+    circleArea = pi*(radius*radius);
+
+// Append your circle answer:
+console.log("The area of the given circle is : " + circleArea);
+
+
+console.log("\n");
+
+
+// triangle area calculation
+   triangleArea = half*base*height;
+
+// Append your triangle answer:
+console.log("The area of the given triangle is : " + triangleArea );
