5bb4cbdb6edb8f6a791765d93bb7e23eaf1406d1
KaraboRamathesele <karaboramathesele8115@gmail.com>
2022-08-17 00:14:27 +0200

    Rainbow Interactive

---
+++ interactiveRainbow.js
@@ -0,0 +1,11 @@
+
+
+function rainbow() {
+    document.getElementById("red").style.backgroundColor = "#ff0000";
+    document.getElementById("orange").style.backgroundColor = "#ff6300";
+    document.getElementById("yellow").style.backgroundColor = "#ffff00";
+    document.getElementById("green").style.backgroundColor = "#008000";
+    document.getElementById("blue").style.backgroundColor = "#0000ff";
+    document.getElementById("purple").style.backgroundColor = "#4b0082";
+    document.getElementById("violet").style.backgroundColor = "#bd31ff";
+}
